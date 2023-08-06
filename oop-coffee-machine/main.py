2e34fc2fdef6feea524f809262f404caa9116fcc
mariam <marhmpa@gmail.com>
2023-08-06 16:59:19 +0330

    Add files via upload
    a coffee machine maker, created by different components in an object-oriented scheme. This project consists of coffee_maker which holds the required data for each type of coffee. The menu and the money machine which gets a hold of processing the coins purchased by the user --- on the main file we run the main loop of program and gathering and implementing all the components

---
+++ oop-coffee-machine/main.py
@@ -0,0 +1,28 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+inp = input('enter your drink espresso latte ')
+is_on = True
+drink = menu.find_drink(inp)
+
+res_sufficient = coffee_maker.is_resource_sufficient(drink)
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f'what would you like{options}')
+    if choice == 'off':
+        is_on = False
+    elif choice =='report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+         drink = menu.find_drink(choice)
+         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+             coffee_maker.make_coffee(drink)
